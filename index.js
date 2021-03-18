45bbed312a4d8a3f3cf294862563439e761cd43a
JorPan <jbpanasewicz@gmail.com>
2021-03-18 15:04:23 -0600

    objection & relation

--- index.js
+++ index.js
@@ -4,12 +4,44 @@ const cors = require("cors");
 const port = process.env.PORT || 9000;
 const config = require("./knexfile")[process.env.NODE_ENV || "development"];
 const database = require("knex")(config);
+const { Model } = require("objection");
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (_, response) => {
+Model.knex(database);
+
+class Pirate extends Model {
+  // static tableName = "pirates"
+  static get tableName() {
+    return "pirates";
+  }
+}
+
+class Ship extends Model {
+  // static tableName = "ships"
+  static get tableName() {
+    return "ships";
+  }
+
+  static relationMappings = {
+    pirates: {
+      relation: Model.HasManyRelation,
+      modelClass: Pirate,
+      join: {
+        from: "ships.id",
+        to: "pirates.ship_id",
+      },
+    },
+  };
+}
+
+app.get("/ships", (_, response) => {
   database("ships").then((ships) => response.json({ ships }));
+});
+
+app.get("/pirates", (_, response) => {
+  database("pirates").then((pirates) => response.json({ pirates }));
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
