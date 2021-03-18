88b4c924ee9b8f3a6e066d68e8041ea849b66454
JorPan <jbpanasewicz@gmail.com>
2021-03-18 14:41:32 -0600

    database, seeds, and home route to ships

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 9000;
+const config = require("./knexfile")[process.env.NODE_ENV || "development"];
+const database = require("knex")(config);
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_, response) => {
-  response.json({ message: 'hello' });
+app.get("/", (_, response) => {
+  database("ships").then((ships) => response.json({ ships }));
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
